c782b00ed3fc0aa18161a79e64c858f1d19cb761
Hamza Kyamanywa <sanshinehamza@gmail.com>
2021-07-19 15:19:12 +0900

    added all files

--- project.js
+++ project.js
@@ -1,1 +1,161 @@
 "use strict";
+const roads = [
+  "Alice's House-Bob's house",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Darcia's House-Ernie's House",
+  "Darcia'House-Town Hall",
+  "Ernei's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      // if we have no yet added this starting point we add it
+      graph[from] = [to];
+    } else {
+      graph[from].push(to); //this starting point already exists and 'to' is added to the array of places we can go from there
+    }
+  }
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    //spliting a string turns it into an array of its parts
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+const roadGraph = buildGraph(roads);
+console.log(roadGraph);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this; //we remain at this place because we cant reach our destination from here
+    } else {
+      //we can reach the destination from where we are , so we go there
+      // we edit the parcels array to that we add parcels from the destination that need to go somewhere else and also leave parcels behind if they have reached their address
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address); // we get out the ones that we should not leave here
+      return new VillageState(destination, parcels); //move to the new desitination with a new updated parcel array
+    }
+  }
+}
+
+let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" },
+]);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+console.log(next.parcels);
+console.log(first.place);
+
+// freezing objects to make them immutable
+let object = Object.freeze({ value: 5 });
+//object.value = 10; -> cannot assign to read only value of the object error
+console.log(object.value);
+
+//randomly choosing places to go to strategy
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+// bad stategy is randomly moving to places to see if you can pick something from a place or have to deliver to the place
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+VillageState.random = function (parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({ place, address });
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+//runRobot(VillageState.random(), randomRobot);
+
+//strategy to follow the route to all places twice to make all deliveries
+const mailRoute = [
+  "Alice's House",
+  "Cabin",
+  "Alice's House",
+  "Bob's House",
+  "Town Hall",
+  "Darcia's House",
+  "Ernie's House",
+  "Grete's House",
+  "Shop",
+  "Grete's House",
+  "Farm",
+  "Marketplace",
+  "Post Office",
+];
+console.log(mailRoute);
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return { direction: memory[0], memory: memory.slice(1) };
+}
+
+//strategy to find shortest routes to where we are supposed to deliver to
+function findRoute(graph, from, to) {
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, route } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concate(place);
+    }
+  }
+}
+
+function goalOrientedRobot({ place, parcels }, route) {
+  if (route.length == 0) {
+    let parcel = parcel[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) };
+}
